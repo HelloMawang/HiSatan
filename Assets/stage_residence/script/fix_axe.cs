bfa461ff5328b4db1b0654728483384191523cfe
JangWony <jwon2@snu.ac.kr>
2015-12-04 17:04:37 +0900

    20151204_1704

---
+++ Assets/stage_residence/script/fix_axe.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fix_axe : MonoBehaviour {
+
+	public GameObject fixed_axe;
+	public GameObject cutton;
+	
+	void Update () {
+		Vector3 lp = cutton.transform.position;
+		float dis = Vector3.Distance (transform.position, lp);
+		
+		if (dis < 0.5f ) {
+			GameObject.Find("EventSystem").GetComponent<on_correcteffect>().on_eff();
+			if(Input.GetKeyUp(KeyCode.Mouse0)){
+				
+				
+				GameObject.Find ("item").GetComponent <manage_inven> ().pop_item(this.gameObject);
+				GameObject.Find ("item").GetComponent <manage_inven> ().pop_item(cutton);
+				
+				GameObject.Find ("item").GetComponent <manage_inven> ().push_item(fixed_axe);
+				GameObject.Find ("blank").GetComponent<inven> ().dragitem = false ;
+			}
+		}
+		
+	}
+}
---
+++ Assets/stage_residence/script/scroll_zoom.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scroll_zoom : MonoBehaviour {
+
+	public GameObject scroll_zoomin;
+	
+	
+	
+	void OnMouseUpAsButton(){
+		if (GameObject.Find ("Boy").GetComponent<player_state> ().his_position == 15) {
+			scroll_zoomin.SetActive (true);
+		}
+	}
+	void Update(){
+		if (GameObject.Find ("Boy").GetComponent<player_state> ().his_position == 15) {
+			if (Input.GetKeyDown (KeyCode.Mouse0)) {
+				scroll_zoomin.SetActive (false);
+			}
+		}
+	}
+}
