eabd76686a0d05354f9b7b54cbd0c6b50bafb619
JangWony <jwon2@snu.ac.kr>
2015-12-10 07:07:22 +0900

    woodman&castle setting, stage1 delete bug

---
+++ Assets/script/worldmap/goto_stage1.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class goto_stage1 : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+			
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+			Application.LoadLevel ("stage1");
+		}
+	
+	}
+}
--- Assets/stage1/script/drawer_touch.cs
+++ Assets/stage1/script/drawer_touch.cs
@@ -4,34 +4,52 @@ using System.Collections;
 public class drawer_touch : MonoBehaviour {
 
 	public GameObject drawer_open;
+	public GameObject empty_open;
 	public GameObject cursor_hand;
 	public GameObject this_item_1;
 	public GameObject this_item_2;
 	public AudioSource open;
 	public AudioSource close;
 
-	private bool check =false ;
-	IEnumerator OnMouseUpAsButton(){
-		if (check == false) {
+	private bool in_item = true;
+	private bool is_open = false;
+	void OnMouseUpAsButton(){
+		if (in_item == true &&is_open==false) {
 			if (GameObject.Find ("Boy").GetComponent<player_state> ().his_position == 2){
-			drawer_open.SetActive (true);
-			check = true;
-			open.Play();
+				drawer_open.SetActive (true);
+				open.Play();
+				is_open = true;
 			}
 
-		} else {
+		} 
+		else if (in_item == false &&is_open==false) {
 			if (GameObject.Find ("Boy").GetComponent<player_state> ().his_position == 2){
-			GameObject.Find ("item").GetComponent <manage_inven>().push_item(this_item_1);
-			GameObject.Find ("item").GetComponent <manage_inven>().push_item(this_item_2);
-			drawer_open.SetActive (false);
-			close.Play ();
+				empty_open.SetActive (true);
+				open.Play();
+				is_open = true;
+			}
+			
+		}
+		else {
+			if (GameObject.Find ("Boy").GetComponent<player_state> ().his_position == 2){
+				if(in_item == true){
+				GameObject.Find ("item").GetComponent <manage_inven>().push_item(this_item_1);
+				GameObject.Find ("item").GetComponent <manage_inven>().push_item(this_item_2);
+				drawer_open.SetActive (false);
+				close.Play ();
+					is_open=false;
+				cursor_hand.SetActive(false);
+				Cursor.visible = true;
+					in_item=false;
+				}
+				else{
+					empty_open.SetActive(false);
+					close.Play();
+					cursor_hand.SetActive(false);
+					Cursor.visible = true;
+					is_open=false;
+				}
 
-			cursor_hand.SetActive(false);
-			Cursor.visible = true;
-			
-			yield return new WaitForSeconds(2f);
-			
-			gameObject.SetActive(false);
 			}
 		}
 	
